fa4aeb7b812627fe43d739e3b162cbdf66a5d84f
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-06-06 18:18:23 +0200

    Remove lock

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.1.1
+Remove lock (for `gem git:` installs and travis)
+
 ## 0.1.0
 Initial release
--- lib/tempfile/fixture/version.rb
+++ lib/tempfile/fixture/version.rb
@@ -2,6 +2,6 @@ require 'tempfile'
 
 Tempfile.class_eval do
   class Fixture
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.1.1'.freeze
   end
 end
